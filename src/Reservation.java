73dd89d6762f27fa479f14f6b0520a1a53ab4e86
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-08-29 22:51:45 +0200

    Create the Reservation class with methods

---
+++ src/Reservation.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+public class Reservation {
+    public int id;
+    public Client client;
+    public Room room;
+    public Date checkIn;
+    public Date checkOut;
+
+    public Reservation(int id, Client client, Room room, Date checkIn, Date checkOut) {
+        this.id = id;
+        this.client = client;
+        this.room = room;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public void cancel() {
+        this.room.setAvailable(true);
+    }
+
+}
