0e958fca7865742956926e05e8fa1dff0fdbf1dd
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-08-29 22:50:11 +0200

    Create the Client class with methods

---
+++ src/Client.java
@@ -0,0 +1,13 @@
+public class Client {
+    public String CIN;
+    public String name;
+    public String email;
+    public String password;
+
+    public Client(String CIN, String name, String email, String password) {
+        this.CIN = CIN;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
